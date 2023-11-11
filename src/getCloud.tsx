4b45b3f98d86c5d189ad5ae6dcf51348e003d307
Yahya Mardini <120016906+gityahco@users.noreply.github.com>
2023-11-11 23:01:06 +0300

    doing sth

--- src/getCloud.tsx
+++ src/getCloud.tsx
@@ -1,5 +1,5 @@
 export default function getCloud(): string[] {
-    return `codespaces elon musk lex fridman atom emacs vim vscode lightweight powerlifting bodybuilding earth mars sun rocket launch fire laptop desktop texteditor`
+    return `codespaces linux macbook windows elon musk lex fridman atom emacs vim vscode lightweight powerlifting bodybuilding earth mars sun rocket launch fire laptop desktop texteditor`
     .split(" ")
     .sort(() => (Math.random() > 0.5 ? 1 : -1));
   }
